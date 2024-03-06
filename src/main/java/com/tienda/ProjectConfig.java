30b29534fdf8cdee4c0bc6c45da932bcbb93b074
kevin <kevin@Kevin>
2024-03-06 14:33:49 -0600

    Finalizacion semana 04

---
+++ src/main/java/com/tienda/ProjectConfig.java
@@ -0,0 +1,52 @@
+package com.tienda;
+
+import java.util.Locale;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class ProjectConfig implements WebMvcConfigurer {
+    /* Los siguientes métodos son para incorporar el tema de internacionalización en el proyecto */
+
+    /* localeResolver se utiliza para crear una sesión de cambio de idioma */
+    @Bean
+    public LocaleResolver localeResolver() {
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.getDefault());
+        slr.setLocaleAttributeName("session.current.locale");
+        slr.setTimeZoneAttributeName("session.current.timezone");
+
+        return slr;
+    }
+    
+    /* localeChangeInterceptor se utiliza para crear un interceptor de cambio de idioma */
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    //Bean para poder acceder a los Messages.properties en código java...
+    @Bean("messageSource")
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource= new ResourceBundleMessageSource();
+        messageSource.setBasenames("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+    
+    
+}
---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    
+    @RequestMapping("/")
+    public String page(Model model) {
+        model.addAttribute("attribute", "value");
+        return "index";
+    }
+    
+}
